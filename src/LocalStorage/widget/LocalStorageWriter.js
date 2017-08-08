d7edebc38facc8e5e87e17a3f8dac9faea2d432c
Conner Charlebois <cdcharlebois@gmail.com>
2017-08-08 10:27:15 -0400

    added attribute on-change listener

--- src/LocalStorage/widget/LocalStorageWriter.js
+++ src/LocalStorage/widget/LocalStorageWriter.js
@@ -24,6 +24,8 @@ define([
         keyName: null,
         onnullMF: null,
         overwrite: null,
+        attribute: null,
+        reWriteOnChange: null,
         // Internal variables.
         _handles: null,
         _contextObj: null,
@@ -38,6 +40,7 @@ define([
 
         update: function(obj, callback) {
             logger.debug(this.id + ".update");
+            // var self = this;
 
             // 3. if not, call onnullMF and..
             // 4. set the value of the item to the result
@@ -50,19 +53,19 @@ define([
                 console.log('The LocalStorage item already exists and we are not overwriting.')
                 return;
             }
-            mx.data.action({
-                params: {
-                    actionname: this.onnullMF,
-                    applyto: "selection",
-                    guids: [obj.getGuid()]
-                },
-                callback: lang.hitch(this, function(res) {
-                    localStorage.setItem(this.keyName, res);
-                }),
-                error: function(err) {
-                    console.log(err)
-                }
-            });
+            if (this.reWriteOnChange === true) {
+                // add handler
+                var _attrHandler = this.subscribe({
+                    guid: obj.getGuid(),
+                    attr: this.attribute,
+                    callback: lang.hitch(this, function(guid, attr, attrValue) {
+                        this._executeMicroflowWriteLS(this.onnullMF, obj.getGuid(), this.keyName);
+                    })
+                })
+                this._handles.push(_attrHandler);
+            }
+            this._executeMicroflowWriteLS(this.onnullMF, obj.getGuid(), this.keyName);
+
 
 
             this._contextObj = obj;
@@ -75,6 +78,27 @@ define([
 
         uninitialize: function() {
             logger.debug(this.id + ".uninitialize");
+        },
+
+        _executeMicroflowWriteLS: function(mfName, guid, keyName) {
+            // return new Promise(function(resolve, reject) {
+            mx.data.action({
+                params: {
+                    actionname: mfName,
+                    applyto: "selection",
+                    guids: [guid]
+                },
+                callback: lang.hitch(this, function(res) {
+                    localStorage.setItem(keyName, res);
+                    // resolve(res)
+                }),
+                error: function(err) {
+                    console.log(err);
+                    // reject(err)
+                }
+            });
+            // });
+
         },
 
         _updateRendering: function(callback) {
